91e7ace7236515cd6c461ff9bc59d4996e96fb5c
antares-me (Anton Salnikov) <antares@antares.me>
2016-06-06 18:57:44 +0300

    Добавлен конфиг Zathura, lightdm и перенесён конфиг zsh

--- README.md
+++ README.md
@@ -12,4 +12,4 @@
 
 `sudo apt-get install cowsay ranger vim vim-nox awesome awesome-extra git zsh
 zsh-common w3m w3m-img curl tmux lightdm ubuntu-session xterm emacs cloc
-libncurses5-dev libncursesw5-dev terminology libssl-dev`
+libncurses5-dev libncursesw5-dev terminology libssl-dev xserver-xephyr`
--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-sudo apt-get install awesome awesome-extra lightdm ubuntu-session xterm vim vim-nox emacs ranger git cloc libncurses5-dev libncursesw5-dev
-ln -s /usr/share/xsessions/awesome.desktop ~/.config/autostart/awesome.desktop
+sudo apt-get install awesome awesome-extra lightdm ubuntu-session xterm vim vim-nox emacs ranger git cloc libncurses5-dev libncursesw5-dev chromium-browser firefox firefox-locale-ru
+mkdir -p ~/.config/autostart/
+ln -s /usr/share/xsessions/awesome.desktop ~/.config/autostart/
+git clone https://github.com/antares-me/dotfiles/
+sudo service lightdm restart
+

63f21938b1b0b547862052499e21682cd92bc244
antares-me (Anton Salnikov) <antares@antares.me>
2016-08-05 11:36:56 +0300

    Добавлена проверка на root

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-sudo apt-get install awesome awesome-extra lightdm ubuntu-session xterm vim vim-nox emacs ranger git cloc libncurses5-dev libncursesw5-dev chromium-browser firefox firefox-locale-ru
+# if [ "$(whoami)" != "root"  ]; then
+#     echo "Sorry, you are not root."
+#     exit 1
+# fi
+
+if [ "$(id -u)" != "0"  ]; then
+    echo "Sorry, you are not root."
+    exit 1
+fi
+
+apt update && apt -y upgrade && apt -y install awesome awesome-extra lightdm \
+    lightdm-webkit-greeter xterm vim vim-nox emacs ranger git cloc \
+    libncurses5-dev libncursesw5-dev chromium-browser firefox firefox-locale-ru
 mkdir -p ~/.config/autostart/
 ln -s /usr/share/xsessions/awesome.desktop ~/.config/autostart/
 git clone https://github.com/antares-me/dotfiles/
-sudo service lightdm restart
+service lightdm restart
 chsh -s /bin/zsh
+

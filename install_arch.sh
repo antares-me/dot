e3d46d92a9934060b5ed29ffcba08aee0185e7db
Anton Salnikov <antares@antares.me>
2017-05-25 12:45:56 +0400

    Обновлён скрипт установки Arch

--- install_arch.sh
+++ install_arch.sh
@@ -1,3 +1,35 @@
 #!/bin/sh
 
+# Менеджер загрузок Aria2 {{{
 pacman -S aria2 
+# }}}
+# Консоль ZSH {{{
+pacman -S zsh
+# }}}
+# Мышь в терминале {{{
+pacman -S gpm
+# }}}
+# Консольный редактор Vim {{{
+pacman -S vim
+# }}}
+# Просмотрщик изображений Fim {{{
+pacman -S fim
+# }}}
+# Система контроля версий Git {{{
+pacman -S git
+# }}}
+# Менеджер конфигураций Stow {{{
+pacman -S stow
+# }}}
+# Терминальный мультиплексор Tmux {{{
+pacman -S tmux
+# }}}
+# PDF читалка Zathura {{{
+pacman -S zathura
+# }}}
+# Менеджер паролей Pass {{{
+pacman -S pass
+# }}}
+# Система бухгалтерского учёта Ledger {{{
+pacman -S ledger
+# }}}
